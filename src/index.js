a7e1d0e186f5c3990995569222a0eb5a98ab3364
Serhii Tkachenko <x.ray868@gmail.com>
2021-07-29 13:39:12 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import './sass/main.scss';
+import foodItemTpl from './templates/food-item.hbs';
+import menu from './menu.json';
+
+const theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const foodMemu = document.querySelector('.js-menu');
+const cardsMarkup = createFoodItemMurkup(menu);
+
+foodMemu.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createFoodItemMurkup(menu) {
+  return foodItemTpl(menu);
+}
+
+const body = document.querySelector('body');
+const themeToggle = document.querySelector('#theme-switch-toggle');
+themeToggle.addEventListener('change', changeTheme);
+
+function changeTheme() {
+  if (body.classList.toggle(theme.DARK)) {
+    return;
+  }
+}
