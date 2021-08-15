71f26eab0f0846072f33e8454a10a82089b763fb
Serhii Tkachenko <x.ray868@gmail.com>
2021-08-15 11:11:07 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
-import defaultExport from './fetchCountries';
+import foodItemTpl from './templates/food-item.hbs';
+import menu from './menu.json';
 
-const refs = {
-  input: document.getElementById('input-js'),
-  countries: document.getElementById('countries-js'),
+const theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
 
-refs.input.addEventListener('input', fetchCountries);
+const foodMemu = document.querySelector('.js-menu');
+const cardsMarkup = createFoodItemMurkup(menu);
+
+foodMemu.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createFoodItemMurkup(menu) {
+  return foodItemTpl(menu);
+}
+
+const themeToggle = document.getElementById('theme-switch-toggle');
+
+themeToggle.addEventListener('change', changeTheme);
+themeToggle.addEventListener('click', addToLocalStorage);
+
+savedTheme();
+
+function changeTheme() {
+  if (document.body.classList.toggle(theme.DARK)) {
+    return;
+  }
+}
+
+function addToLocalStorage() {
+  if (themeToggle.checked) {
+    localStorage.setItem('theme', theme.DARK);
+  } else {
+    localStorage.setItem('theme', theme.LIGHT);
+  }
+}
+
+function savedTheme() {
+  const themeInLocalStorage = localStorage.getItem('theme');
+
+  if (themeInLocalStorage) {
+    document.body.classList.add(themeInLocalStorage);
+    if (themeInLocalStorage === theme.DARK) {
+      themeToggle.checked = true;
+    }
+  }
+}
